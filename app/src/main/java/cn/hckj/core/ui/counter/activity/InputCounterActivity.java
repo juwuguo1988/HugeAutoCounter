449f028ac6f39afa5a561cd7027fc47a19e5252c
Juwuguo1988 <Juwuguo@xinzhili.cn>
2020-03-18 23:49:04 +0800

    修改录音按钮位置

--- app/src/main/java/cn/hckj/core/ui/counter/activity/InputCounterActivity.java
+++ app/src/main/java/cn/hckj/core/ui/counter/activity/InputCounterActivity.java
@@ -1,8 +1,10 @@
 package cn.hckj.core.ui.counter.activity;
 
+import android.Manifest;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -11,18 +13,28 @@ import android.widget.Toast;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.iflytek.cloud.RecognizerResult;
+import com.iflytek.cloud.SpeechConstant;
+import com.iflytek.cloud.SpeechError;
+import com.iflytek.cloud.SpeechUtility;
+import com.iflytek.cloud.ui.RecognizerDialog;
+import com.iflytek.cloud.ui.RecognizerDialogListener;
+
 import java.util.ArrayList;
 
 import cn.hckj.core.R;
 import cn.hckj.core.common.bean.UserGoodBean;
+import cn.hckj.core.common.bean.VoiceRecordBean;
 import cn.hckj.core.ui.counter.adapter.GoodDisplayAdapter;
+import cn.hckj.core.utils.RxPermissionUtils;
 
 /**
  * Created by juwuguo on 2020-03-11.
  */
 public class InputCounterActivity extends AppCompatActivity {
+    private static final String TAG = "InputCounterActivity";
     private EditText et_good_name, et_good_dose, et_good_unit;
-    private Button btn_add, btn_create_counter;
+    private Button btn_add, btn_create_counter, btn_start_record;
     private ListView lv_good_display;
     private ArrayList<UserGoodBean> userGoodBeans = new ArrayList<>();
     private GoodDisplayAdapter mGoodDisplayAdapter;
@@ -31,6 +43,8 @@ public class InputCounterActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input_counter_layout);
+        // 将“xxxxxxx”替换成您申请的 APPID
+        SpeechUtility.createUtility(this, SpeechConstant.APPID + "=5e6ede86");
         findViewById();
         setListener();
     }
@@ -40,6 +54,7 @@ public class InputCounterActivity extends AppCompatActivity {
         et_good_dose = findViewById(R.id.et_good_dose);
         et_good_unit = findViewById(R.id.et_good_unit);
         btn_add = findViewById(R.id.btn_add);
+        btn_start_record = findViewById(R.id.btn_start_record);
         btn_create_counter = findViewById(R.id.btn_create_counter);
         lv_good_display = findViewById(R.id.lv_good_display);
         mGoodDisplayAdapter = new GoodDisplayAdapter(this, userGoodBeans, true);
@@ -74,6 +89,48 @@ public class InputCounterActivity extends AppCompatActivity {
             startActivity(intent);
         });
 
+        btn_start_record.setOnClickListener(v -> {
+            RxPermissionUtils.getInstance(this).getPowerStatus(() -> {
+                        startSpeechDialog();
+                    },
+                    Manifest.permission.READ_PHONE_STATE,
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.RECORD_AUDIO,
+                    Manifest.permission.READ_CONTACTS
+            );
+        });
+
+    }
+
+
+    /**
+     * 初始化语音识别
+     */
+    private void startSpeechDialog() {
+        //1.创建RecognizerDialog对象
+        RecognizerDialog mDialog = new RecognizerDialog(this, null);
+        //2.设置accent、language等参数
+        mDialog.setParameter(SpeechConstant.LANGUAGE, "zh_cn");
+        mDialog.setParameter(SpeechConstant.ACCENT, "mandarin");
+        //3.设置回调接口
+        mDialog.setListener(new RecognizerDialogListener() {
+            @Override
+            public void onResult(RecognizerResult recognizerResult, boolean isLast) {
+                if (!isLast) {
+                    //解析语音
+                    //返回的result为识别后的汉字,直接赋值到TextView上即可
+                    String result = VoiceRecordBean.parseVoiceContent(recognizerResult.getResultString());
+                    Log.e(TAG, "=======result========>" + result);
+                }
+            }
+
+            @Override
+            public void onError(SpeechError speechError) {
+                Toast.makeText(InputCounterActivity.this, speechError.getErrorDescription(), Toast.LENGTH_LONG).show();
+            }
+        });
+        //4.显示dialog，接收语音输入
+        mDialog.show();
     }
 
 
